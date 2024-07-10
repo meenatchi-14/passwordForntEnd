93a4db617afe7fa45a701ed8e112568e9e7c10a2
meenatchi <meenatchicse44@gmail.com>
2024-07-10 12:44:02 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import Home from './pages/Home.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import ForgetPassword from './pages/ForgetPassword.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+
+
+function App() {
+  return (
+   <>
+   <BrowserRouter>
+   <Routes>
+
+<Route path='/signup' element={<Signup/>}/>
+
+<Route path='/login' element={<Login/>}/>
+
+<Route path='/home' element={<Home/>}/>
+
+<Route path='/forgetpassword' element={<ForgetPassword/>}/>
+
+<Route path='/reset-password/:randomString/:expirationTimestamp' element={<ResetPassword/>}/>
+
+<Route path='*' element={<Login/>}/>
+
+   </Routes>
+   
+   </BrowserRouter>
+   
+   </>
+  )
+}
+
+export default App
