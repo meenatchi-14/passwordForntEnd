c82888be4d27e1e864840598b72cc19848eb6306
meenatchi <meenatchicse44@gmail.com>
2024-08-05 19:16:30 +0530

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import ForgetPassword from './pages/ForgetPassword.jsx'
-
+import ResetPassword from './pages/ResetPassword.jsx'
 
 
 function App() {
@@ -20,6 +20,8 @@ function App() {
 <Route path='/home' element={<Home/>}/>
 
 <Route path='/forgetpassword' element={<ForgetPassword/>}/>
+
+<Route path='/reset-password/:randomString/:expitationTimestamp' element={<ResetPassword/>}/>
 <Route path='*' element={<Login/>}/>
 
    </Routes>
